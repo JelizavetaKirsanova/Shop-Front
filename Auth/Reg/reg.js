07a3f1ac66100b95ad0e87b43dc46f19bbde8563
lizaK2006 <kirsanovaliza2019@gmail.com>
2023-02-25 23:51:59 +0200

    l

--- Auth/Reg/reg.js
+++ Auth/Reg/reg.js
@@ -1,42 +1,55 @@
 window.addEventListener("load", () => {
-    const button = document.getElementById("reg");
-    const name = document.getElementById("name");
-    const email = document.getElementById("email");
-    const passwords = document.getElementsByClassName("password");
+  const button = document.getElementById("reg");
+  const name = document.getElementById("name");
+  const email = document.getElementById("email");
+  const passwords = document.getElementsByClassName("password");
+
+  if (document.cookie.includes("email")) {
+    console.log(document.cookie);
+    const form = document.getElementById("form");
+    const linkBack = document.getElementById("linkBack");
+    form.style.display = "none";
+    linkBack.style.display = "block";
+  } else {
     button.addEventListener("click", () => {
-        if (
-            name.value &&
-            email.value &&
-            passwords[0].value &&
-            passwords[0].value == passwords[1].value
-        ) {
-            const user = {
-                name: name.value,
-                email: email.value,
-                password: passwords[0].value,
-            };
-            
-            fetch("http://localhost:3000/reg", {
-                method: "POST",
-                body: JSON.stringify(user),
-                headers: {
-                    "Content-Type": "application/json",
-                },
-            }).then(
-                (response) => {
-                    console.log(response)
-                    if (response.ok) {
-                        alert("done");
-                    }else{
-                        alert('error')
-                    }
-                },
-                (error) => {
-                    console.log(error);
-                }
-            );
-        } else {
-            alert("Check your data again");
-        }
+      if (
+        name.value &&
+        email.value &&
+        passwords[0].value &&
+        passwords[0].value == passwords[1].value
+      ) {
+        const user = {
+          name: name.value,
+          email: email.value,
+          password: passwords[0].value,
+        };
+
+        fetch("http://localhost:3000/reg", {
+          method: "POST",
+          body: JSON.stringify(user),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }).then(
+          (response) => {
+            console.log(response);
+            if (response.ok) {
+              alert("done");
+              response.json().then((data) => {
+                console.log(data)
+                document.cookie = `email=${data.user.email}`;
+              });
+            } else {
+              alert("error");
+            }
+          },
+          (error) => {
+            console.log(error);
+          }
+        );
+      } else {
+        alert("Check your data again");
+      }
     });
+  }
 });
