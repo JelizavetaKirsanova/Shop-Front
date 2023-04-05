b02c86102f39495464185d20b65093a19d7335ce
lizaK2006 <kirsanovaliza2019@gmail.com>
2023-04-05 16:20:38 +0300

    hw

--- script.js
+++ script.js
@@ -1,37 +1,69 @@
 window.addEventListener("load", async () => {
-    if(await AuthCheck()){
-        document.getElementById("buttons").style.display = "none"
-        fetch("http://localhost:3000/goods", {
-            method: "POST",
-            body: JSON.stringify({ token: document.cookie, category : "default" }),
-            headers: {
-                "Content-Type": "application/json",
-            },
-        })
-            .then((response) => response.json())
-            .then((res) => {
-                console.log(res)
-                if (res.status == "ok") {
-                    let divApi = document.getElementById("root");
+  if (await AuthCheck()) {
+    document.getElementById("buttons").style.display = "none";
+    fetch("http://localhost:3000/goods", {
+      method: "POST",
+      body: JSON.stringify({ token: document.cookie, category: "default" }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((res) => {
+        console.log(res);
+        if (res.status == "ok") {
+          let divApi = document.getElementById("root");
 
+          let container = document.createElement("div");
 
-                    
-                    let container = document.createElement("div");
-                    container.classList.add("grid")
-                    console.log(res.body);
-                    for (let good of res.goods) {
-                        container.innerHTML += `<div>
+          container.innerHTML += `<h2>${"Default"}</h2>`;
+
+          container.classList.add("grid");
+          console.log(res.body);
+          for (let good of res.goods) {
+            container.innerHTML += `<div>
                             <h2>${good.title}</h2>
                             <p>${good.description}</p>
                             <b>${good.price}</b>
                             </div>`;
-                    }
-                    divApi.append(container);
-                } else {
-                    console.log(res)
-                    alert("Please login or register");
-                }
-            });
-    }
-    
+          }
+          divApi.append(container);
+        } else {
+          console.log(res);
+          alert("Please login or register");
+        }
+      });
+
+    fetch("http://localhost:3000/goods", {
+      method: "POST",
+      body: JSON.stringify({ token: document.cookie, category: "animals" }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((res) => {
+        console.log(res);
+        if (res.status == "ok") {
+          let divApi = document.getElementById("root");
+
+          let container = document.createElement("div");
+          container.innerHTML += `<h2>${"Animals"}</h2>`;
+
+          container.classList.add("grid");
+          console.log(res.body);
+          for (let good of res.goods) {
+            container.innerHTML += `<div>
+                            <h2>${good.title}</h2>
+                            <p>${good.description}</p>
+                            <b>${good.price}</b>
+                            </div>`;
+          }
+          divApi.append(container);
+        } else {
+          console.log(res);
+          alert("Please login or register");
+        }
+      });
+  }
 });
