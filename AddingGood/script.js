964dc4adb9c8914e72d8396e4e0227e54bb9bc00
lizaK2006 <kirsanovaliza2019@gmail.com>
2023-04-18 16:11:53 +0300

    hw

--- AddingGood/script.js
+++ AddingGood/script.js
@@ -45,24 +45,6 @@ window.addEventListener("load", async () => {
 }
 
 
-  async function loadCategories(){
-      return fetch("http://localhost:3000/categories", {
-          method: "POST",
-          body: JSON.stringify({ token: document.cookie}),
-          headers: {
-            "Content-Type": "application/json",
-          },
-        })
-          .then((response) => response.json())
-          .then((res) => {
-            console.log(res);
-            if (res.status == "ok") {
-              return res.categories.map(category => category.category)
-            } else {
-              console.log(res);
-              alert("Please login or register");
-            }
-          });
-  }
+  
   
   
