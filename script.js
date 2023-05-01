f06b0a14c75f260c60031dd1d04b24c957008949
lizaK2006 <kirsanovaliza2019@gmail.com>
2023-05-01 19:42:17 +0300

    hw

--- script.js
+++ script.js
@@ -1,69 +1,52 @@
-
 window.addEventListener("load", async () => {
   if (await AuthCheck()) {
     document.getElementById("buttons").style.display = "none";
-    loadCategories().then(catList => 
-        catList.forEach(cat => loadGoodsByCat(cat))
-    )
+    let root = document.getElementById("root");
+    const params = new URLSearchParams(window.location.search);
+    const id = params.get("id");
+    let title = document.createElement("h2")
+    title.innerText = "USERS"
+    root.append(title)
+    loadUsers();
+
+
   }
 });
 
-function loadGoodsByCat(cat){
-    fetch("http://localhost:3000/goods", {
-        method: "POST",
-        body: JSON.stringify({ token: document.cookie, category: cat}),
-        headers: {
-          "Content-Type": "application/json",
-        },
-      })
-        .then((response) => response.json())
-        .then((res) => {
-          console.log(res);
-          if (res.status == "ok") {
-            let divApi = document.getElementById("root");
-            let category = document.createElement("div")
-            let container = document.createElement("div");
+function loadUsers() {
+  fetch("http://localhost:3000/user", {
+    method: "POST",
+    body: JSON.stringify({ token: document.cookie }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => response.json())
+    .then((res) => {
+      console.log(res);
+      if (res.status == "ok") {
+        let divApi = document.getElementById("root");
+        let users = document.createElement("div");
+        let container = document.createElement("div");
 
-            category.classList.add("category")
-  
-            category.innerHTML += `<h2>${cat.charAt(0).toUpperCase() + cat.slice(1)}</h2>`;
-  
-            container.classList.add("grid");
-            for (let good of res.goods) {
-                console.log(good)
-              container.innerHTML += `<a href="/Good/index.html?id=${good.ID} ">
-                              <h3>${good.title}</h3>
-                              <p>${good.description}</p>
-                              <p>${good.price}</p>
-                              </a>`;
-            }
-            
-            category.append(container);
-            divApi.append(category);
-          } else {
-            console.log(res);
-            alert("Please login or register");
-          }
-        });
-}   
+        users.classList.add("users");
 
-async function loadCategories(){
-    return fetch("http://localhost:3000/categories", {
-        method: "POST",
-        body: JSON.stringify({ token: document.cookie}),
-        headers: {
-          "Content-Type": "application/json",
-        },
-      })
-        .then((response) => response.json())
-        .then((res) => {
-          console.log(res);
-          if (res.status == "ok") {
-            return res.categories.map(category => category.category)
-          } else {
-            console.log(res);
-            alert("Please login or register");
-          }
-        });
+        container.classList.add("grid");
+        console.log(res.user);
+        for (let user of res.user) {
+          console.log(user);
+          container.innerHTML += `<div class="user">
+                                  <h3>${user.name}</h3>
+                                  <p>${user.email}</p>
+                                  <p>${user.password}</p>
+                                  </div>`;
+        }
+
+        users.append(container);
+        divApi.append(users);
+      } else {
+        console.log(res);
+        alert("Please login or register");
+      }
+    });
 }
-
